6eee85d376cbe8183e8ec082a4cd70a1a6c3a8c1
Nathan Brittain <github@enjibby.dev>
2023-06-17 00:55:56 +1000

    Style slot add button to show where new block content will go

--- src/components/hosts/SlotHost.vue
+++ src/components/hosts/SlotHost.vue
@@ -13,5 +13,5 @@ const emit = defineEmits(['add']);
 
 <template>
     <slot></slot>
-    <div v-if="selected" :style="{ position: 'absolute', top: '100%', left: 0, right: 0, textAlign: 'center', display: 'block' }"><div @click.prevent="emit('add')" :style="{ marginTop: '-1rem', borderRadius: '9999px', backgroundColor: '#22C55E', paddingLeft: '1rem', paddingRight: '1rem', paddingTop: '.25rem', paddingBottom: '.25rem', fontSize: '.75rem', lineHeight: '1rem', color: '#fff', width: 'fit-content', display: 'inline-block' }">add +</div></div>
+    <div v-if="selected"><div @click.prevent="emit('add')" :style="{ borderRadius: '0.25rem', borderColor: '#22C55E', borderWidth: '0.125rem', padding: '0.125rem', fontSize: '2rem', fontWeight: '700', borderStyle: 'dashed', color: '#22C55E', maxWidth: '100%', height: '100%', textAlign: 'center' }">+</div></div>
 </template>
